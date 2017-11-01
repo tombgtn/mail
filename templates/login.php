fc47e2b1069465eb19daa8e536f1ca7e7a5b778f
Tom Baumgarten <github@tombgtn.com>
2017-11-01 09:49:06 +0100

    Essai de lien interne au template (assets)

--- templates/login.php
+++ templates/login.php
@@ -5,4 +5,4 @@
 */
 ?>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ad, autem et nam? Expedita saepe numquam cumque itaque architecto perferendis sit, suscipit iure cum quas sapiente deleniti dolores earum dicta?</p>
-<img src="assets/img/profil.jpg" alt="assets/img/profil.jpg"/>
+<img src="profil.jpg" alt="profil.jpg"/>
