7faef940e5328d0e9fe89352ea7c704e4ba57d30
Tom Baumgarten <github@tombgtn.com>
2017-11-01 23:16:06 +0100

    Reecriture d'url

--- templates/login.php
+++ templates/login.php
@@ -5,4 +5,5 @@
 */
 ?>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ad, autem et nam? Expedita saepe numquam cumque itaque architecto perferendis sit, suscipit iure cum quas sapiente deleniti dolores earum dicta?</p>
-<img src="profil.jpg" alt="profil.jpg"/>
+<img src="profil.jpg" alt="profil.jpg"/><br/>
+<img src="templates/profil.jpg" alt="templates/profil.jpg"/>
