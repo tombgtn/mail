650671c35024c6df207fb5a49d24a68c9f2f62dd
Tom Baumgarten <github@tombgtn.com>
2017-12-01 18:04:29 +0100

    Un autre test

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/*
 if (defined('PROCESSING')) { die('You shall not pass !'); }
 else { define('PROCESSING', true); }
 
@@ -8,5 +8,36 @@ require_once './app/init.php';
 init();
 
 require_once './modules/front.php';
+*/
 
 
+
+abstract class MODULE
+{
+	public final function __call($method, $parameters) {
+		var_dump($method);
+		var_dump($parameters);
+	}
+}
+
+class Helpers extends MODULE
+{
+	private function test() {
+		echo "hello world";
+	}
+}
+
+class Front extends MODULE
+{
+	private function plouf() {
+		$h = new Helpers();
+		$h->test();
+		Helpers::test();
+	}
+}
+
+function init() {
+	Front::plouf();
+}
+
+init();
